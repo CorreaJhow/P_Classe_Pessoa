86000aed060be3b5251a0b9e1a46c5697cd77f7e
Jhonatas Rodrigues Correa <jhonatasrcorrea@gmail.com>
2022-08-23 23:37:59 -0300

    Adicionar arquivos de projeto.

---
+++ PClasse_Pessoa/Pessoa.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PClasse_Pessoa
+{
+    internal class Pessoa
+    {
+        String nome, cpf, dataNascimento, telefone;
+
+        public void setNome(String nome)
+        {
+            this.nome = nome;
+        }
+
+        public void setData(String dataNascimento)
+        {
+            this.dataNascimento = dataNascimento;
+        }
+
+        public void setDocumento(String cpf)
+        {
+            this.cpf = cpf;
+        }
+
+        public void setTelefone(String telefone)
+        {
+            this.telefone = telefone;
+        }
+
+        public String getNome()
+        {
+            return nome;
+        }
+
+        public String getData()
+        {
+            return dataNascimento;
+        }
+
+        public String getDocumento()
+        {
+            return cpf;
+        }
+
+        public String getTelefone()
+        {
+            return telefone;
+        }
+    }
+}
